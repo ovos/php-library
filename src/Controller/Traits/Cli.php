30b204aead818f60ff92b27824fa6757006fdff7
mg <mg@ovos.at>
2019-02-25 20:42:05 +0100

    updates to ovos lib

--- src/Controller/Traits/Cli.php
+++ src/Controller/Traits/Cli.php
@@ -92,7 +92,7 @@ trait Cli
 
 		Terminal::output('<darkgray>[' . $this->getPid() . '] '
 			. '<purple>' . date('Y-m-d H:i:s') . ': '
-			. '<reset>' . $message . '<reset>' . PHP_EOL, $this->_coloredOutput);
+			. '<reset>' . $message . '<reset>' . PHP_EOL);
 			
 		ob_flush();
 	}
