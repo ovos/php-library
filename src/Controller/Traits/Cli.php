35c270516dea7e78cfbe27e37c6003622e2f11f8
mg <mg@ovos.at>
2019-04-10 12:34:52 +0200

    fix for readLine

--- src/Controller/Traits/Cli.php
+++ src/Controller/Traits/Cli.php
@@ -93,7 +93,5 @@ trait Cli
 		Terminal::output('<darkgray>[' . $this->getPid() . '] '
 			. '<purple>' . date('Y-m-d H:i:s') . ': '
 			. '<reset>' . $message . '<reset>' . PHP_EOL, $this->_coloredOutput);
-			
-		ob_flush();
 	}
 }
