4d5fa2554e648514ab06ac8afbbafbea6bf756f8
mg <mg@ovos.at>
2019-07-12 23:06:24 +0200

    added modules

--- src/Translator.php
+++ src/Translator.php
@@ -59,7 +59,7 @@ class Translator
 	/**
 	 * @return Translation[]
 	 */
-	public function getPaths(): array
+	public function getTranslations(): array
 	{
 		return $this->_translations;
 	}
@@ -73,7 +73,7 @@ class Translator
 	public function translate(string $phrase, ...$params): string
 	{
 		$translation = $phrase;
-
+		
 		foreach($this->_translations as $translationAdapter)
 		{
 			$result = $translationAdapter->translate($phrase);
@@ -84,24 +84,7 @@ class Translator
 			}
 		}
 
-		return $this->getTranslation($translation, ...$params);
-	}
-
-	/**
-	 * @param string $translation
-	 * @param mixed $params
-	 *
-	 * @return string
-	 */
-	protected function getTranslation(string $translation, ...$params): string
-	{
-		if(empty($params))
-		{
-			return $translation;
-		}
-		
-		$formatter = new MessageFormatter($this->_locale->getLanguage(), $translation);
-		return $formatter->format($params);
+		return $this->_getTranslation($translation, ...$params);
 	}
 
 	/**
@@ -131,6 +114,23 @@ class Translator
 			}
 		}
 
-		return $this->getTranslation($translation, ...$params);
+		return $this->_getTranslation($translation, ...$params);
+	}
+
+	/**
+	 * @param string $translation
+	 * @param mixed $params
+	 *
+	 * @return string
+	 */
+	protected function _getTranslation(string $translation, ...$params): string
+	{
+		if(empty($params))
+		{
+			return $translation;
+		}
+		
+		$formatter = new MessageFormatter($this->_locale->getLanguage(), $translation);
+		return $formatter->format($params);
 	}
 }
