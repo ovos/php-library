2cc53d1070412cb0418fb4c9f6798a7b881ae8d5
mg <mg@ovos.at>
2019-07-24 19:41:24 +0200

    Response/CSV

---
+++ src/Response/Csv.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Ovos\Response;
+
+/**
+ * Csv
+ *
+ * @package Ovos
+ * @author Marcin Gil <mg@ovos.at>
+ */
+class Csv extends Html
+{
+	/**
+	 * Construct
+	 *
+	 * @param string $response
+	 */
+	public function __construct(string $response = null)
+	{
+		parent::__construct($response);
+
+		$this->setHeader('Content-Type', 'text/csv; charset=utf-8');
+	}
+
+	/**
+	 * @param string $filename
+	 */
+	public function setFilename(string $filename)
+	{
+		$this->setHeader('Content-Disposition',  'attachment;filename=' . $filename);
+	}
+}
