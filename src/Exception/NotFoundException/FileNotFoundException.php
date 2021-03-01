503a2bdee55d6f98cf3b1c9f057d958f8bcda94b
Marcin Gil <mg@ovos.at>
2021-03-01 21:27:26 +0100

    php 8.0 rewrite

---
+++ src/Exception/NotFoundException/FileNotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Ovos\Exception\NotFoundException;
+
+use Ovos\Exception\NotFoundException;
+
+/**
+ * FileNotFoundException
+ *
+ * @package Ovos
+ * @author Marcin Gil <mg@ovos.at>
+ */
+class FileNotFoundException extends NotFoundException
+{
+}
