78ddc83fca70669f0c32ca4b5255a37badaf75ca
Marcin Gil <mg@ovos.at>
2022-05-30 15:34:57 +0200

    updates for php-lib 3.0

--- src/Translator.php
+++ src/Translator.php
@@ -93,7 +93,7 @@ class Translator
 				$this->_translations) === false)
 			{
 				$this->_translations[$translationPath]
-					= new Translation($this->_locale, $translationPath);
+					= new Translation($this, $translationPath);
 			}
 		}
 		
@@ -143,7 +143,8 @@ class Translator
 	 */
 	public function translatePlural(string $phraseSingular, string $phrasePlural, int $n, ...$params): string
 	{
-		$translation = null;
+		$translation = $this->getPlural($n) === 0 // english
+			? $phraseSingular : $phrasePlural;
 
 		foreach($this->_translations as $translationAdapter)
 		{
@@ -157,11 +158,6 @@ class Translator
 			{
 				$translation = $result;
 			}
-		}
-		
-		if($translation === null) // missing in .po file
-		{
-			return '<!-- missing -->';
 		}
 
 		return $this->_getTranslation($translation, ...$params);
@@ -200,5 +196,79 @@ class Translator
 	public static function getTranslationsPaths(): array
 	{
 		return self::$_translationsPaths;
+	}
+	
+	/**
+	 * @see Zend_Translate_Plural
+	 * Alternative is to parse and eval "Plural-Forms:" header in .mo file
+	 * 
+	 * Returns the plural definition to use
+	 * 
+	 * @param int $number
+	 *
+	 * @return int
+	 */
+	public function getPlural(int $number): int
+	{
+		$language = $this->_locale->language;
+		
+		if($this->_locale->symbol === 'pt_BR') // exception for Brasil
+		{
+			$language = 'xbr';
+		}
+		
+		return match($language)
+		{
+			'az', 'bo', 'dz', 'id', 'ja', 'jv', 'ka', 'km', 'kn', 'ko', 'ms', 'th', 'tr', 'vi', 'zh'
+				=> 0,
+			'af', 'bn', 'bg', 'ca', 'da', 'de', 'el', 'en', 'eo', 'es', 'et', 'eu', 'fa', 'fi', 'fo', 'fur', 'fy',
+			'gl', 'gu', 'ha', 'he', 'hu', 'is', 'it', 'ku', 'lb', 'ml', 'mn', 'mr', 'nah', 'nb', 'ne', 'nl', 'nn',
+			'no', 'om', 'or', 'pa', 'pap', 'ps', 'pt', 'so', 'sq', 'sv', 'sw', 'ta', 'te', 'tk', 'ur', 'zu'
+				=> ($number === 1) ? 0 : 1,
+			'am', 'bh', 'fil', 'fr', 'gun', 'hi', 'ln', 'mg', 'nso', 'xbr', 'ti', 'wa'
+				=> (($number === 0) || ($number === 1)) ? 0 : 1,
+			'be', 'bs', 'hr', 'ru', 'sr', 'uk'
+				=> (($number % 10 === 1) && ($number % 100 !== 11)) ? 0
+					: ((($number % 10 >= 2) && ($number % 10 <= 4) && (($number % 100 < 10) || ($number % 100 >= 20))) ? 1 : 2),
+			'cs', 'sk'
+				=> ($number === 1) ? 0
+					: ((($number >= 2) && ($number <= 4)) ? 1 : 2),
+			'ga'
+				=> ($number === 1) ? 0 : (($number === 2) ? 1 : 2),
+			'lt'
+				=> (($number % 10 === 1) && ($number % 100 !== 11)) ? 0
+					: ((($number % 10 >= 2) && (($number % 100 < 10) || ($number % 100 >= 20))) ? 1 : 2),
+			'sl'
+				=> ($number % 100 === 1) ? 0
+					: (($number % 100 === 2) ? 1
+						: ((($number % 100 === 3) || ($number % 100 === 4)) ? 2 : 3)),
+			'mk'
+				=> ($number % 10 === 1) ? 0 : 1,
+			'mt'
+				=> ($number === 1) ? 0
+					: ((($number === 0) || (($number % 100 > 1) && ($number % 100 < 11))) ? 1
+						: ((($number % 100 > 10) && ($number % 100 < 20)) ? 2 : 3)),
+			'lv'
+				=> ($number === 0) ? 0
+					: ((($number % 10 === 1) && ($number % 100 !== 11)) ? 1 : 2),
+			'pl'
+				=> ($number === 1) ? 0
+					: ((($number % 10 >= 2) && ($number % 10 <= 4) && (($number % 100 < 12) || ($number % 100 > 14))) ? 1 : 2),
+			'cy'
+				=> ($number === 1) ? 0
+					: (($number === 2) ? 1
+						: ((($number === 8) || ($number === 11)) ? 2 : 3)),
+			'ro'
+				=> ($number === 1) ? 0
+					: ((($number === 0) || (($number % 100 > 0) && ($number % 100 < 20)))
+						? 1 : 2),
+			'ar'
+				=> ($number === 0) ? 0
+					: (($number === 1) ? 1
+						: (($number === 2) ? 2
+							: ((($number >= 3) && ($number <= 10)) ? 3
+								: ((($number >= 11) && ($number <= 99)) ? 4 : 5)))),
+			default => 0,
+		};
 	}
 }
--- src/Translator/CachedAdapter.php
+++ src/Translator/CachedAdapter.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace Ovos\Translator;
 
 use Ovos\ArrayObject;
-use Ovos\Locale;
 use Ovos\Strings;
 
 use function Ovos\services;
@@ -22,23 +21,23 @@ use function strlen;
 class CachedAdapter
 {
 	/**
-	 * @var Locale
+	 * @var Translation
 	 */
-	protected Locale $_locale;
-
+	protected Translation $_translation;
+	
 	/**
 	 * @var array
 	 */
 	protected array $_translations = [];
 	
 	/**
-	 * @param Locale $locale
-	 * @param string $filename
+	 * @param Translation $translation
 	 */
-	public function __construct(Locale $locale, string $filename)
+	public function __construct(Translation $translation)
 	{
-		$this->setLocale($locale);
+		$this->_translation = $translation;
 	
+		$filename = $translation->getPath();
 		if(file_exists($filename) === false)
 		{
 			return;
@@ -130,9 +129,9 @@ class CachedAdapter
 		}
 
 		$result = $this->gettext($key);
-
+		
 		// find out the appropriate form
-		$select = $this->getPlural($number);
+		$select = $this->_translation->getTranslator()->getPlural($number);
 
 		$list = explode(chr(0), $result);
 		if(isset($list[$select]) === false)
@@ -163,97 +162,6 @@ class CachedAdapter
 		}
 
 		return $ret;
-	}	
-	
-	/**
-	 * @see Zend_Translate_Plural
-	 * Alternative is to parse and eval "Plural-Forms:" header in .mo file
-	 * 
-	 * Returns the plural definition to use
-	 * 
-	 * @param int $number
-	 *
-	 * @return int
-	 */
-	public function getPlural(int $number): int
-	{
-		$language = $this->_locale->language;
-	
-		if($this->_locale->symbol === 'pt_BR') // exception for Brasil
-		{
-			$language = 'xbr';
-		}
-		
-		return match ($language)
-		{
-			'az', 'bo', 'dz', 'id', 'ja', 'jv', 'ka', 'km', 'kn', 'ko', 'ms', 'th', 'tr', 'vi', 'zh' => 0,
-			'af', 'bn', 'bg', 'ca', 'da', 'de', 'el', 'en', 'eo', 'es', 'et', 'eu', 'fa', 'fi', 'fo', 'fur', 'fy', 'gl', 'gu', 'ha', 'he', 'hu', 'is', 'it', 'ku', 'lb', 'ml', 'mn', 'mr', 'nah', 'nb', 'ne', 'nl', 'nn', 'no', 'om', 'or', 'pa', 'pap', 'ps', 'pt', 'so', 'sq', 'sv', 'sw', 'ta', 'te', 'tk', 'ur', 'zu' => ($number === 1)
-				? 0 : 1,
-			'am', 'bh', 'fil', 'fr', 'gun', 'hi', 'ln', 'mg', 'nso', 'xbr', 'ti', 'wa' => (($number === 0) || ($number === 1))
-				? 0 : 1,
-			'be', 'bs', 'hr', 'ru', 'sr', 'uk' => (($number % 10 === 1) && ($number % 100 !== 11))
-				? 0
-				: ((($number % 10 >= 2) && ($number % 10 <= 4) && (($number % 100 < 10) || ($number % 100 >= 20)))
-					? 1 : 2),
-			'cs', 'sk' => ($number === 1) ? 0
-				: ((($number >= 2) && ($number <= 4)) ? 1 : 2),
-			'ga' => ($number === 1) ? 0 : (($number === 2) ? 1 : 2),
-			'lt' => (($number % 10 === 1) && ($number % 100 !== 11))
-				? 0
-				: ((($number % 10 >= 2) && (($number % 100 < 10) || ($number % 100 >= 20)))
-					? 1 : 2),
-			'sl' => ($number % 100 === 1) ? 0
-				: (($number % 100 === 2) ? 1
-					: ((($number % 100 === 3) || ($number % 100 === 4)) ? 2
-						: 3)),
-			'mk' => ($number % 10 === 1) ? 0 : 1,
-			'mt' => ($number === 1)
-				? 0
-				: ((($number === 0) || (($number % 100 > 1) && ($number % 100 < 11)))
-					? 1
-					: ((($number % 100 > 10) && ($number % 100 < 20)) ? 2 : 3)),
-			'lv' => ($number === 0) ? 0
-				: ((($number % 10 === 1) && ($number % 100 !== 11)) ? 1 : 2),
-			'pl' => ($number === 1)
-				? 0
-				: ((($number % 10 >= 2) && ($number % 10 <= 4) && (($number % 100 < 12) || ($number % 100 > 14)))
-					? 1 : 2),
-			'cy' => ($number === 1) ? 0
-				: (($number === 2) ? 1
-					: ((($number === 8) || ($number === 11)) ? 2 : 3)),
-			'ro' => ($number === 1)
-				? 0
-				: ((($number === 0) || (($number % 100 > 0) && ($number % 100 < 20)))
-					? 1 : 2),
-			'ar' => ($number === 0)
-				? 0
-				: (($number === 1)
-					? 1
-					: (($number === 2) ? 2
-						: ((($number >= 3) && ($number <= 10)) ? 3
-							: ((($number >= 11) && ($number <= 99)) ? 4 : 5)))),
-			default => 0,
-		};
-	}
-	
-	/**
-	 * @param Locale $locale
-	 * 
-	 * @return self
-	 */
-	public function setLocale(Locale $locale): self
-	{
-		$this->_locale = $locale;
-		
-		return $this;
-	}
-	
-	/**
-	 * @return Locale
-	 */
-	public function getLocale(): Locale
-	{
-		return $this->_locale;
 	}
 	
 	/**
