948c949e0d751097343f89d5d38e0b142e2ede15
Marcin Gil <mg@ovos.at>
2023-03-30 18:59:27 +0200

    added Service\Auth (for extending it in project)

---
+++ src/Service/Auth.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace Ovos\Service;
+
+use Models\User;
+use Ovos\Exception;
+use Ovos\Service;
+
+/**
+ * Auth
+ *
+ * @package Ovos
+ * @author Marcin Gil <mg@ovos.at>
+ */
+class Auth extends Service
+{
+	/**
+	 * @var string
+	 */
+	public const SYMBOL = 'auth';
+	
+	/**
+	 * @var ?User
+	 */
+	protected ?User $_user = null;
+	
+	/**
+	 * @return string
+	 */
+	public function getSymbol(): string
+	{
+		return self::SYMBOL;
+	}
+
+	/**
+	 * @param ?User $user
+	 *
+	 * @return self
+	 */
+	public function setUser(?User $user): self
+	{
+		$this->_user = $user;
+		
+		return $this;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasUser(): bool
+	{
+		return $this->_user !== null;
+	}
+
+	/**
+	 * @return ?User
+	 */
+	public function getUser(): ?User
+	{
+		return $this->_user;
+	}
+}
