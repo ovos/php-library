363e98850a3549d4d0d43a8331933d0b2c07efb8
mg <mg@ovos.at>
2023-05-10 21:00:46 +0200

    Fix loader, add Stream Factory class

---
+++ src/Stream/Request/Factory.php
@@ -0,0 +1,92 @@
+<?php
+declare(strict_types=1);
+
+namespace Ovos\Stream\Request;
+
+use Ovos\Arrays;
+use Ovos\Measurement;
+use Ovos\Response;
+use Ovos\Stream\Request;
+
+/**
+ * Factory
+ * Use this factory to group types of requests (URL & Context)
+ *
+ * @package Ovos
+ * @author Marcin Gil <mg@ovos.at>
+ */
+class Factory
+{
+	/**
+	 * @var string
+	 */
+	protected string $_baseUrl;
+	
+	/**
+	 * @var array
+	 */
+	protected array $_defaultContextOptions = [];	
+	
+	/**
+	 */
+	public function __construct(string $baseUrl,
+		array $defaultContextOptions = [],
+	)
+	{
+		$this->_baseUrl = $baseUrl;
+		$this->_defaultContextOptions = $defaultContextOptions;
+	}
+	
+	/**
+	 * @param string $url
+	 * @param array $contextOptions
+	 *
+	 * @return Request
+	 */
+	public function create(string $url, array $contextOptions = []): Request
+	{
+		$requestUrl = $this->_baseUrl . $url;
+		$requestContextOptions = Arrays::deepMerge($this->_defaultContextOptions, $contextOptions);
+		return new Request($requestUrl, $requestContextOptions);
+	}
+
+	/**
+	 * @param string $baseUrl
+	 *
+	 * @return self
+	 */
+	public function setBaseUrl(string $baseUrl)
+	{
+		$this->_baseUrl = $baseUrl;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getBaseUrl(): string
+	{
+		return $this->_baseUrl;
+	}
+
+	/**
+	 * @param array $defaultContextOptions
+	 *
+	 * @return self
+	 */
+	public function setDefaultContextOptions(array $defaultContextOptions): self
+	{
+		$this->_defaultContextOptions = $defaultContextOptions;
+
+		return $this;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getDefaultContextOptions(): array
+	{
+		return $this->_defaultContextOptions;
+	}
+}
