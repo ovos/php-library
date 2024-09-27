a9a10c9c7671c332354be10791fa4d9200be0d8c
Marcin Gil <mg@ovos.at>
2024-09-27 22:54:37 +0200

    new Store Traits: MemoryService & Find

---
+++ src/Store/Traits/Find.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace Ovos\Store\Traits;
+
+use Ovos\Model;
+use Ovos\Service\Memory;
+use Ovos\Services;
+
+/**
+ * Find
+ *
+ * @package Ovos
+ * @author Marcin Gil <mg@ovos.at>
+ */
+trait Find
+{
+	/**
+	 * @param ...$arguments
+	 *
+	 * @return false|Model|Model[]
+	 */
+	public function find(...$arguments): false|Model|array
+	{
+		$many = $arguments['many'] ?? false;
+		unset($arguments['many']);
+		
+		$query = $this->executeFind(...$arguments);
+		
+		return $many
+			? $query->fetchAll(PDO::FETCH_CLASS, $this->getModel())
+			: $query->fetchObject($this->getModel());
+	}
+}
