a0712a2de898479d13eb290ebd90e9e320bda1d3
Marcin Gil <mg@ovos.at>
2025-01-14 17:54:43 +0100

    NotHuman validator (trap for bots)

---
+++ src/Form/Validator/NotHuman.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Ovos\Form\Validator;
+
+use Ovos\Form\Error;
+use Ovos\Form\Validator;
+
+/**
+ * NotHuman
+ *
+ * @package Ovos
+ * @author Marcin Gil <mg@ovos.at>
+ */
+class NotHuman extends Validator
+{
+	/**#@+
+	 * Error constants
+	 */
+	public const ERROR_NOT_HUMAN = 'not_human';
+	/**#@-*/
+	
+	/**
+	 * @var string[]
+	 */
+	protected array $_messages =
+	[
+		self::ERROR_NOT_HUMAN => '"%s" is not valid.',
+	];
+	
+	/**
+	 * @param null|mixed $value
+	 *
+	 * @return bool
+	 */
+	public function isValid(mixed $value): bool
+	{
+		$valid = $value === '';
+		if($valid === false)
+		{
+			$error = new Error(self::ERROR_NOT_HUMAN, sprintf($this->getMessage(self::ERROR_NOT_HUMAN),
+				$this->getElement()->getName()
+			));
+			$this->addError($error);
+		}
+		
+		return $valid;
+	}
+}
