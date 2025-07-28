e5a017f21de90d6158ade2cebddf92ee80ed8957
Marcin Gil <mg@ovos.at>
2025-07-28 21:20:28 +0200

    reworked tests & benchmarks to be more efficient -> __construct is only called once, there are now 3 methods that can be used additionally: prepare, finalize and deconstruct -> added missing files

---
+++ src/Exception/InvalidException/InvalidClassException.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Ovos\Exception\InvalidException;
+
+use Ovos\Exception\InvalidException;
+
+/**
+ * InvalidClassException
+ *
+ * @package Ovos
+ * @author Marcin Gil <mg@ovos.at>
+ */
+class InvalidClassException extends InvalidException
+{
+}
---
+++ src/Exception/InvalidException/InvalidConfigException.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Ovos\Exception\InvalidException;
+
+use Ovos\Exception\InvalidException;
+
+/**
+ * InvalidConfigException
+ *
+ * @package Ovos
+ * @author Marcin Gil <mg@ovos.at>
+ */
+class InvalidConfigException extends InvalidException
+{
+}
